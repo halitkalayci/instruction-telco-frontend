82fe412475bd27327077ab3869cad85ab68c0f80
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-10-29 16:32:09 +0300

    Add reactive form

--- src/app/components/listview/listview.component.ts
+++ src/app/components/listview/listview.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 import { CategoriesService } from './../../services/categories.service';
 import { Category } from 'src/app/models/category';
@@ -20,6 +25,8 @@ export class ListviewComponent implements OnInit {
 
   categoryIdToDelete: number = 0; // state
 
+  error: string = '';
+
   //Angular IoC (Inversion of Control) Container kullanır.
   //Dependency Injection (Bağımlılık Enjeksiyonu)
   constructor(
@@ -34,12 +41,12 @@ export class ListviewComponent implements OnInit {
 
   createCategoryAddForm() {
     this.categoryAddForm = this.formBuilder.group({
-      name: '',
-      description: '',
+      name: ['', Validators.required],
+      description: ['', [Validators.required, Validators.minLength(10)]],
     });
   }
 
-  getCategories() {
+  getCategories(): void {
     // Object tipi henüz belli olmayan referans tip diyebiliriz. Referans tiplerin en temel sınıfı diyebiliriz.
     this.categoriesService.getCategories().subscribe((response) => {
       // Observer Design Pattern
@@ -47,19 +54,45 @@ export class ListviewComponent implements OnInit {
     });
   }
 
-  changecategoryIdToDelete(event: any) {
-    this.categoryIdToDelete = event.target.value;
-  }
+  // changecategoryIdToDelete(event: any) {
+  //   this.categoryIdToDelete = event.target.value;
+  // }
 
-  add() {
-    console.log(this.categoryAddForm.value);
+  add(): void {
+    if (this.categoryAddForm.invalid) {
+      this.error = 'Form is invalid';
+      return;
+    }
+    if (this.error) this.error = '';
 
-    // let category: Category = {
-    //   id: categoryIdToAdd,
-    //   name: categoryNameToAdd,
-    //   description: categoryDescriptionToAdd,
+    // const {name, description} = this.categoryAddForm.value;
+    // // this.categoryAddForm.value
+    // const category: Category = {
+    //   id: 0,
+    //   // name: name,
+    //   name,
+    //   description,
     // };
-    // this.categoriesService.add(category);
+
+    // spread operator ... (ES6)
+    const category: Category = {
+      ...this.categoryAddForm.value,
+    };
+    this.categoriesService.add(category).subscribe({
+      next: (response) => {
+        console.info(`Category(${response.id}) has added.`);
+      },
+      error: (err) => {
+        console.log(err);
+
+        this.error = err.statusText;
+      },
+      complete: () => {
+        if (this.error) this.error = '';
+        this.categoryAddForm.reset();
+        this.getCategories();
+      },
+    });
   }
 
   delete() {
