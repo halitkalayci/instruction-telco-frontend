2b065f7e3d439b4dc80ad364dc3bd1c3949b35d1
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-11-05 16:48:46 +0300

    Add reading user name in token user model

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { TokenUserModel } from 'src/app/models/tokenUserModel';
 
 @Component({
   selector: 'app-navbar',
@@ -14,12 +16,18 @@ export class NavbarComponent implements OnInit {
   //* EventEmitter sadece componentler arası, HTML tarafında iletişim için kullanılır.
   @Output() onLogout = new EventEmitter<void>();
   @Output() onLogoutWithValue = new EventEmitter<string>();
-  // tokenUserModel: TokenUserModel | null = null;
+  tokenUserModel$: Observable<TokenUserModel | null>;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.tokenUserModel$ = this.authService.tokenUserModel$;
+  }
 
   ngOnInit(): void {
     this.handleOnLogin();
+    //* TS tarafında subscribe olunabilir.
+    // this.tokenUserModel$.subscribe((tokenUserModel) => {
+    //   if (tokenUserModel) this.isLogin = true;
+    // });
   }
 
   logout() {
